ebd63e090c96e98966bacb8aea4c1167c0aeda0a
Nem-git <loukastjean06@gmail.com>
2024-12-11 21:02:45 -0500

    now regex works completely fine

--- common/parameters.py
+++ common/parameters.py
@@ -1,4 +1,5 @@
 import argparse
+import re
 
 from .options import Options
 
@@ -6,7 +7,7 @@ from .options import Options
 class Parameters:
 
 
-    def Parse(args: list[str], options: Options):
+    def Parse(self, args: list[str], options: Options):
 
         # Parameters using -
         
@@ -27,12 +28,20 @@ class Parameters:
                 options.audio_description = True
         
         parser = argparse.ArgumentParser(description="Tool to download media from multiple online services")
+        
+        parser.add_argument("download")
+        
+        
         download_group = parser.add_argument_group("Download", "Arguments for the downloads", prefix_chars="-")
         
-        parser.add_argument("--latest", "-l", help="Select only the latest episode", action="store_true", default=False, type=bool)
+        download_group.add_argument("--latest", "-l", help="Downloads only the latest episode", action="store_true", default=False, type=bool, required="download" in args)
+        download_group.add_argument("--subtitles", "-s", help="Downloads all subtitles available", action="store_true", default=False, type=bool, required="download" in args)
+        download_group.add_argument("--audio-description", "-ad", help="Downloads audio description audio", action="store_true", default=False, type=bool, required="download" in args)
+        download_group.add_argument("--quiet", "-q", help="Don't receive output on the terminal", action="store_true", default=False, type=bool, required="download" in args)
+        download_group.add_argument("--resolution", "-r", help="Tries to download video at screen height given", action="store", default=1080, type=int, required="download" in args)
         
-        
-        
+
+
         #parser.add
 
         
@@ -41,105 +50,33 @@ class Parameters:
 
 
     def parse_season_episode(seasons_episodes: str):
-        seasons_episodes = seasons_episodes.lower()
-        start_season = 0
-        end_season = 0
-        start_episode = 0
-        end_episode = 0
 
+        # ^s(\d+)
+        # Search for a season number
+        # e?(\d+)?
+        # Search for an episode number
+        # -?s?(\d+)?
+        # Search for the ending season
+        # e?(\d+)?
+        # Search for the ending episode
 
-        if "s" in seasons_episodes:
-            number_of_s = 0
+        seasons_episodes: list[tuple[str]] = re.findall(r"^s(\d+)e?(\d+)?-?s?(\d+)?e?(\d+)?$", seasons_episodes)
 
-            for char in seasons_episodes:
-                if char == "s":
-                    number_of_s += 1
+        if len(seasons_episodes) < 4:
+            return None
 
-            s_index = seasons_episodes.index("s")
-            final_number = ""
-
-            for number in range(s_index + 1, len(seasons_episodes) - s_index):
-                try:
-                    int(seasons_episodes[number])
-                    final_number += seasons_episodes[number]
-                except:
-                    break
-                
-                start_season = int(final_number)
-
-            if number_of_s == 2:
-                end_season_seasons_episodes = seasons_episodes.split("-")[1]
-                s_index = end_season_seasons_episodes.index("s")
-                final_number = ""
-
-                for number in range(s_index + 1, len(end_season_seasons_episodes) - s_index):
-                    try:
-                        int(end_season_seasons_episodes[number])
-                        final_number += end_season_seasons_episodes[number]
-                    except:
-                        break
-                    
-                    end_season = int(final_number)
-
-                if end_season < start_season:
-                    print("You can't have a smaller ending season number than starting season number")
-                    exit()
-            else:
-                end_season = start_season
-
-        else:
+        start_season = seasons_episodes[0][0]
+        start_episode = seasons_episodes[0][1]
+        end_season = seasons_episodes[0][2]
+        end_episode = seasons_episodes[0][3]
+        
+        if start_episode == "":
             start_episode = 0
+        
+        if end_season == "":
             end_season = 999
-            start_episode = 0
-            end_episode = 999
-
-            return start_season, end_season, start_episode, end_episode
-
-
-        #EPISODES
-        if "e" in seasons_episodes:
-            number_of_e = 0
-
-            for char in seasons_episodes:
-                if char == "e":
-                    number_of_e += 1
-
-            e_index = seasons_episodes.index("e")
-            final_number = ""
-
-            # STARTING EPISODE
-            for number in range(e_index + 1, len(seasons_episodes)):
-                try:
-                    int(seasons_episodes[number])
-                    final_number += seasons_episodes[number]
-                except:
-                    break
-                
-                start_episode = int(final_number)
-
-            # FINAL EPISODE
-            if number_of_e == 2:
-                end_episode_seasons_episodes = seasons_episodes.split("-")[1]
-                e_index = end_episode_seasons_episodes.index("e")
-                final_number = ""
-
-                for number in range(e_index + 1, len(end_episode_seasons_episodes)):
-                    try:
-                        int(end_episode_seasons_episodes[number])
-                        final_number += end_episode_seasons_episodes[number]
-                    except:
-                        break
-                    
-                    end_episode = int(final_number)
-
-                if start_season == end_season and start_episode > end_episode:
-                    print("You can't have a smaller ending episode number than starting episode number in the same season")
-                    exit()
-            else:
-                end_episode = start_episode
-
-        else:
-            start_episode = 0
+        
+        if end_episode == "":
             end_episode = 999
 
         return start_season, end_season, start_episode, end_episode
